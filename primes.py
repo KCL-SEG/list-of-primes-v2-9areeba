87c2478db9411a404691c7981ad49d8d30aa8926
9areeba <areeba-bito@hotmail.com>
2022-10-08 17:13:13 +0100

    Handle Negative values

--- primes.py
+++ primes.py
@@ -2,5 +2,32 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
+    if number_of_primes <= 0:
+        raise ValueError("Number of primes need to be more than 0!")
+        
     list = []
+    list.append(2)
+
+    while len(list) < number_of_primes:
+        foundNextPrime = False
+        nextNumberToCheck = list[-1]
+
+        while(not foundNextPrime):
+            nextNumberToCheck += 1
+            foundNextPrime = isPrime(nextNumberToCheck)
+
+        list.append(nextNumberToCheck)
+
     return list
+
+
+def isPrime(number):
+    prime = True
+    i = 2
+
+    while i < number and prime:
+        if(number % i == 0):
+            prime = False
+        i += 1
+
+    return prime
